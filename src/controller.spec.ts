4105890e888346935893646bdfd8801a30d9d6ec
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2021-09-27 10:45:09 -0300

    chore: add jest and fix husky

--- src/controller.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Name of the group', () => {
-  it('should 1 to be 1', () => {
-    expect(1).toBe(1)
-  })
-})
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+function testingFunction (): number {
+  return 1
+}
+
+testingFunction()
